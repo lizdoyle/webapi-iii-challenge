14fd458463e478c9d779c4915190c206e9acee01
lizdoyle <lizdoyle1121@gmail.com>
2019-11-07 23:13:02 -0500

    PORT FIX

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // code away!
-
+require('dotenv').config();
 const server = require('./server');
 
-  server.listen(3000, () =>
-  console.log('\n===Server running on http://localhost:3000===\n')
-);
+
+// const {PORT} = process.env
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
+});
